0be1cc63375cdaa4e4aece9bbbc1ba6745f48997
Tiago <tiagoesmartins930@gmail.com>
2026-09-13 15:08:20 +0200

    Finish theme for all and fix the animal delete screen

--- Delete_Animal.aspx.designer.cs
+++ Delete_Animal.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace AQUACORE_CMPG223
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-
-        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
@@ -42,13 +33,22 @@ namespace AQUACORE_CMPG223
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// txtAnimalID control.
+        /// ddlAnimalID control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtAnimalID;
+        protected global::System.Web.UI.WebControls.DropDownList ddlAnimalID;
+
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
 
         /// <summary>
         /// btnDeleteAnimal control.
